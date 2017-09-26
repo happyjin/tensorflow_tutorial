af5a000cf57cf440a4dfb4a72838b9ad5d52b927
Jin Li <engineer.tony.lee@gmail.com>
2017-09-26 21:23:11 +0200

    print result

--- mnist_begin.py
+++ mnist_begin.py
@@ -16,7 +16,7 @@ x = tf.placeholder(tf.float32, [None, 784]) # None means that a dimension can be
 W = tf.Variable(tf.zeros([784, 10])) # initialize this variable with zeros
 b = tf.Variable(tf.zeros([10]))
 # step 2: implement model (model with no specific data in it)
-#y = tf.nn.softmax(tf.matmul(x, W) + b)
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 # step 3: implement cross-entropy (model with no specific data in it)
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=tf.matmul(x, W) + b, labels=y_) # more stable numerically
@@ -32,3 +32,11 @@ for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     # run train_step feeding in the batches data to replace the placeholders
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# evaluate the model
+# step 1: count correct prediction
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+# step 2: compute accuracy
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# step 3: print accuracy from graph, we need to apply sess.run in order to print result of test dataset
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
