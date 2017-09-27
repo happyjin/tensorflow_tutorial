77682cb10a96f106dd640f488e63eb0a7390a265
Jin Li <engineer.tony.lee@gmail.com>
2017-09-27 12:48:05 +0200

    download datasets

--- iris_NN.py
+++ iris_NN.py
@@ -13,3 +13,13 @@ IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 
 IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
+
+if not os.path.exists(IRIS_TRAINING):
+  raw = urllib.urlopen(IRIS_TRAINING_URL).read()
+  with open(IRIS_TRAINING,'w') as f:
+    f.write(raw)
+
+if not os.path.exists(IRIS_TEST):
+  raw = urllib.urlopen(IRIS_TEST_URL).read()
+  with open(IRIS_TEST,'w') as f:
+    f.write(raw)
