cffd08bbc82637f7136316e808a24a93ac80e146
Jin Li <engineer.tony.lee@gmail.com>
2017-09-27 12:46:27 +0200

    add dataset route

---
+++ iris_NN.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import urllib
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+# Data sets
+IRIS_TRAINING = "iris_training.csv"
+IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
+
+IRIS_TEST = "iris_test.csv"
+IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
