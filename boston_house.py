ad250a5fb9430a497a174ac50f6cbc47be83b6a6
Jin Li <engineer.tony.lee@gmail.com>
2017-09-27 17:05:33 +0200

    step 9

--- boston_house.py
+++ boston_house.py
@@ -22,4 +22,31 @@ prediction_set = pd.read_csv("datasets/boston_predict.csv", skipinitialspace=Tru
 # step 3: create a list of FeatureColumn for the input data which specify the set of features to use for training
 feature_cols = [tf.feature_column.numeric_column(k) for k in FEATURES]
 
-# step 4:
+# step 4: instantiate a DNNRegressor for the neural network regression model
+regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
+                                      hidden_units=[10, 10],
+                                      model_dir="/Users/lijin/Desktop/tmp")
+
+# step 5: build the input_fn. pass input data into regressor
+def get_input_fn(data_set, num_epochs=None, shuffle=True):
+    return tf.estimator.inputs.pandas_input_fn(
+        x = pd.DataFrame({k: data_set[k].values for k in FEATURES}), # pass any data_set include training_set, test_set
+        y = pd.Series(data_set[LABEL].values),
+        # num_epochs controls the number of epochs to iterate over data. For training, set this to None
+        # input_fn will iterate over once and then raise OutOfRangeError
+        num_epochs=num_epochs,
+        # For evaluate and predict, set this to False so that iterate over the data sequentially
+        # For train, set this to True
+        shuffle=shuffle)
+
+# step 6: train the neural network regressor
+regressor.train(input_fn=get_input_fn(training_set), steps=5000)
+
+# step 7: evaluate the model
+ev = regressor.evaluate(input_fn=get_input_fn(test_set, num_epochs=1, shuffle=False))
+
+# step 8: retrieve the loss from the evaluation results and print it to output
+loss_score = ev["loss"]
+print("Loss: {0:f}".format(loss_score))
+
+# step 9:
