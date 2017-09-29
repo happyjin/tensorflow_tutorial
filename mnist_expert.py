9096792140541fa2e0fc23a23a5e59fc4955a3aa
Jin Li <engineer.tony.lee@gmail.com>
2017-09-29 10:40:39 +0200

    add comments

--- mnist_expert.py
+++ mnist_expert.py
@@ -32,56 +32,65 @@ if __name__ == "__main__":
     x = tf.placeholder(tf.float32, shape=[None, 784])
     y_ = tf.placeholder(tf.float32, [None, 10])
     ### build the CNN model
-    # step 1: the first convolutional layer
+    # step 2: the first convolutional layer
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     # reshape the data into 4D tensor
     x_image = tf.reshape(x, [-1, 28, 28, 1]) # [-1, image_width, image_height, number of color channels]
 
-    # step 1.2: convolve x_image with the weight tensor, add the bias, apply ReLU funtion, and finally max pool
+    # step 2.2: convolve x_image with the weight tensor, add the bias, apply ReLU funtion, and finally max pool
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    # step 2: the second convolutional layer
+    # step 3: the second convolutional layer
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
 
-    # step 2.2: convolve x_image with the weight tensor, add the bias, apply ReLU funtion, and finally max pool
+    # step 3.2: convolve x_image with the weight tensor, add the bias, apply ReLU funtion, and finally max pool
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    # step 3: Densely Connected Layer (fully-connected layer)
+    # step 4: Densely Connected Layer (fully-connected layer)
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
 
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-    # step 4: Dropout to reduce overfitting
+    # step 5: Dropout to reduce overfitting
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-    # step 5: Readout Layer, just like for the one layer softmax regression above.
+    # step 6: Readout Layer, just like for the one layer softmax regression above.
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
     ### train and evaluate the model
+    # step 1: set how to account loss and accuracy using cross_entropy in this case
     cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
     correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    # step 2: pass real data(for placeholder) into model and train the model using session
     with tf.Session() as sess:
+        # step 2.1: initialize all variables before using them
         sess.run(tf.global_variables_initializer())
-        for i in range(20000):
+        # step 3: set training maximum training steps
+        for i in range(500):
+            # step 3.1: set batch_size
             batch = mnist.train.next_batch(50)
             if i % 100 == 0:
+                # step 3.3: evaluate the model after every 100 steps
                 train_accuracy = accuracy.eval(feed_dict={
                     x: batch[0], y_: batch[1], keep_prob: 1.0})
+                # step 3.4: print current result under current batch data
                 print('step %d, training accuracy %g' % (i, train_accuracy))
+            # step 3.2: train the model and feed the current batch data
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
+        # step 4:
         print('test accuracy %g' % accuracy.eval(feed_dict={
             x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
